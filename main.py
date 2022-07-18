a92216a449161feb1c0b9c2d19c93cbb1d3418ef
Tukeque <TukequeMail@gmail.com>
2022-07-18 17:16:17 +0200

    add camera & scrolling

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tyler import Tyler, Sprite, Scene
 from math import ceil, floor
 from pygame.locals import *
-from worlds import worlds, solids
 import loader
 import pygame
 
@@ -57,9 +56,79 @@ class Player:
         (0.999, -0.1)
     ]
 
-    def __init__(self, name: str, x: int, y: int):
+    def __init__(self, name: str, x: float, y: float) -> None:
         self.name = name
         self.x, self.y = x, y
+
+class Camera:
+    def __init__(self, x: float, y: float, tyler: Tyler) -> None:
+        self.x, self.y = x, y
+        self.tyler = tyler
+
+        self.old_x = self.x + self.tyler.tile_w
+        self.old_y = self.y + self.tyler.tile_h
+        self.changed = False
+
+    def get_poses(self) -> tuple[tuple[int, int], tuple[int, int], tuple[int, int]]:
+        stop_x_off = False
+        stop_y_off = False
+
+        if self.x < 0:
+            self.x = 0
+        elif self.x >= (loader.world["size"][X] - 1) * self.tyler.tile_w:
+            self.x = (loader.world["size"][X] - 1) * self.tyler.tile_w
+            stop_x_off = True
+
+        if self.y < 0:
+            self.y = 0
+        elif self.y >= (loader.world["size"][Y] - 1) * self.tyler.tile_h:
+            self.y = (loader.world["size"][Y] - 1) * self.tyler.tile_h
+            stop_y_off = True
+
+        main_x = floor(self.x / self.tyler.tile_w) * self.tyler.tile_w
+        sec_x = main_x + self.tyler.tile_w
+        off_x = self.x % self.tyler.tile_w
+
+        main_y = floor(self.y / self.tyler.tile_h) * self.tyler.tile_h
+        sec_y = main_y + self.tyler.tile_h
+        off_y = self.y % self.tyler.tile_h
+
+        if stop_x_off:
+            off_x = 0
+
+        if stop_y_off:
+            off_y = 0
+
+        if main_x != self.old_x or main_y != self.old_y:
+            self.changed = True
+        else:
+            self.changed = False
+
+        if sec_x >= (loader.world["size"][X] - 1) * self.tyler.tile_w:
+            sec_x = (loader.world["size"][X] - 1) * self.tyler.tile_w
+
+        if sec_y >= (loader.world["size"][Y] - 1) * self.tyler.tile_h:
+            sec_y = (loader.world["size"][Y] - 1) * self.tyler.tile_h
+
+        self.old_x = main_x
+        self.old_y = main_y
+
+        return ((floor(main_x), floor(main_y)), (floor(sec_x), floor(sec_y)), (-off_x, -off_y))
+
+    def pre_draw(self, blocks: list[str]) -> tuple[float, float]:
+        main, sec, off = self.get_poses()
+
+        if self.changed:
+            for x in range(self.tyler.tile_w):
+                for y in range(self.tyler.tile_h):
+                    self.tyler.get_tile(self.tyler.backgrounds[0], x, y).texture_index = self.tyler.texture(blocks[loader.get_tile(main[X] + x, main[Y] + y)])
+                    if loader.world["size"][X] > 1: 
+                        self.tyler.get_tile(self.tyler.backgrounds[1], x, y).texture_index = self.tyler.texture(blocks[loader.get_tile(sec[X] + x, main[Y] + y)])
+                    if loader.world["size"][Y] > 1:
+                        self.tyler.get_tile(self.tyler.backgrounds[2], x, y).texture_index = self.tyler.texture(blocks[loader.get_tile(main[X] + x, sec[Y] + y)])
+                        self.tyler.get_tile(self.tyler.backgrounds[3], x, y).texture_index = self.tyler.texture(blocks[loader.get_tile(sec[X] + x, sec[Y] + y)])
+
+        return off
 
 class Main(Scene):
     tyler: Tyler
@@ -149,11 +218,19 @@ class Main(Scene):
             self.player.dy -= self.player.gravity * delta
 
     def start(self) -> None:
-        self.tyler.fill(self.tyler.backgrounds[0], self.tyler.texture(DIRT))
         loader.load_world("assets/levels/level00.json")
 
-        self.player = Player("pepe", 3, 4)
+        self.tyler.fill(self.tyler.backgrounds[0], self.tyler.texture(AIR))
+        if loader.world["size"][X] > 1:
+            self.tyler.fill(self.tyler.backgrounds[1], self.tyler.texture(AIR))
+        if loader.world["size"][Y] > 1:
+            self.tyler.fill(self.tyler.backgrounds[2], self.tyler.texture(AIR))
+            self.tyler.fill(self.tyler.backgrounds[3], self.tyler.texture(AIR))
+
+        self.player = Player("pepe", 1, 3)
+        self.camera = Camera(0, 0, self.tyler)
         self.tyler.sprites["player"] = Sprite(self.tyler.texture(PLAYER), self.player.x, self.player.y, 1, self.tyler)
+        self.camera.pre_draw(self.BLOCKS) # first update
 
     def event(self, event) -> None:
         match event.type:
@@ -162,17 +239,40 @@ class Main(Scene):
                     self.player.dy += self.player.jump
 
     def draw(self) -> None:
-        for x in range(self.tyler.tile_w):
-            for y in range(self.tyler.tile_h):
-                self.tyler.get_tile(self.tyler.backgrounds[0], x, y).texture_index = self.tyler.texture(self.BLOCKS[loader.get_tile(x, y)])
+        off = self.off
 
-        self.tyler.draw(0, 0, self.tyler.backgrounds[0])
+        if loader.world["size"][Y] > 1: 
+            self.tyler.draw(
+                off[X] + self.tyler.tile_w,
+                off[Y] + self.tyler.tile_h,
+            self.tyler.backgrounds[3])
+
+            self.tyler.draw(
+                off[X],
+                off[Y] + self.tyler.tile_h,
+            self.tyler.backgrounds[2])
+
+        if loader.world["size"][X] > 1: 
+            self.tyler.draw(
+                off[X] + self.tyler.tile_w,
+                off[Y],
+            self.tyler.backgrounds[1])
+
+        self.tyler.draw(
+            off[X],
+            off[Y],
+        self.tyler.backgrounds[0])
 
     def loop(self, delta) -> None:
-        self.tyler.sprites["player"].x = cool_effect(self.player.x)
-        self.tyler.sprites["player"].y = cool_effect(self.player.y)
-
         self.do_player_movement(delta)
+
+        self.camera.x = self.player.x - 7.5
+        self.camera.y = self.player.y - 7.5
+
+        self.off = self.camera.pre_draw(self.BLOCKS)
+
+        self.tyler.sprites["player"].x = self.player.x - self.camera.x
+        self.tyler.sprites["player"].y = self.player.y - self.camera.y
 
 plattyler: Plattyler = Plattyler(768, 768, 16, 16, {
     "main": Main()
